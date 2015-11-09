88e0d93def662c150023488d0d87245c299a6de8
ojasp <opanwar@gmail.com>
2015-11-09 16:47:24 -0500

    Update deploy_code.ps1

--- deploy_code.ps1
+++ deploy_code.ps1
@@ -1,25 +1,69 @@
 #Replace Write-Host to Write-Output to export the output to a file
 
+param(
+    [string]$SiteName = "TestOjas",                           #<---------Name of the website in IIS
+    [string]$WebsitePath = "http://localhost:8090/",          #<---------Port number to check if website is online
+    [string]$AppPoolName = "Test--Ojas",                      #<---------Name of the Application Pool
+    [string]$public="", #"_PublishedWebsites\Public"          #<---------Relative path pf public website; relative to build folder as asked below (old_build_folder/new_build_folder)
+    [string]$PSEmailServer = "onewire-web06.ny.fsvs.com",     #<---------This variable sets SMTP server. It's a system variable.
+    [string]$old_build_folder = "",                           #<---------This variable saved the old build folder that is entered by user
+    [string]$new_build_folder = ""                            #<---------This variable saved the new build folder that is entered by user
+)
+
 $deploymentresult = @()                                       #<---------This variable saves the output of commands & is added as body in the email
 $Webconfigcopyerror=@()                                       #<---------This variable saves the error(if any) when copying web.config
 $jsoncopyerror=@()                                            #<---------This variable saves the error(if any) when copying client_secret.json
-$SiteName = "Website1"                                        #<---------Name of the website in IIS
-$WebsitePath = "http://localhost:8090/"                       #<---------Port number to check if website is online
-$AppPoolName = "DefaultSiteAppPool"                           #<---------Name of the Application Pool
+#$SiteName = "TestOjas"                                        #<---------Name of the website in IIS
+#$WebsitePath = "http://localhost:8090/"                       #<---------Port number to check if website is online
+#$AppPoolName = "Test--Ojas"                                   #<---------Name of the Application Pool
 $build_folder="C:\inetpub\wwwroot\"                           #<---------Not needed?
-$public=""                                                    #<---------Relative path pf public website; relative to build folder as asked below (old_build_folder/new_build_folder)
+#$public="" #"_PublishedWebsites\Public"                       #<---------Relative path pf public website; relative to build folder as asked below (old_build_folder/new_build_folder)
 $globalerror = 0                                              #<---------This variable tracks if error(s) occured during the script
-$PSEmailServer = "smtp.blahblah.com"                          #<---------This variable sets SMTP server. It's a system variable.
+$PSEmailServer = "onewire-web06.ny.fsvs.com"                  #<---------This variable sets SMTP server. It's a system variable.
 $timeelapsedmonitoring = 0                                    #<---------This variable is used to see how many seconds have elapsed since the start of monitoring for number of current active connections
 $buildcopyerror=@()                                           #<---------This variable is used to track if an error occured when changing physical directory
+$wordstoskip = 0                                              #<---------This variable is used to filter out all the other details when pulling number of current connections counters
 
-$deploymentresult += Write-Output "`nStarting deployment on $env:computername"
-$old_build_folder= Read-Host 'Old Build Folder excluding trailing slash'
-$new_build_folder= Read-Host 'New Build Folder excluding trailing slash'
+## CHANGE STRING LENGTH 60 TO USE SITE Name            ---Done
+## MOVE RECYCLE                                        ---Done
 
+$deploymentresult += Write-Output "`nStarting deployment on $env:computername, here are the user-entered parameters:"
+#$old_build_folder= Read-Host 'Old Build Folder excluding trailing slash'
+#$new_build_folder= Read-Host 'New Build Folder excluding trailing slash'
 
-$deploymentresult += Write-Output "`nYou entered:`nOld Build Folder: $old_build_folder"
-$deploymentresult += Write-Output "New Build Folder: $new_build_folder"
+$deploymentresult += Write-Output "SiteName: $SiteName"
+$deploymentresult += Write-Output "WebsitePath: $WebsitePath"
+$deploymentresult += Write-Output "AppPoolName: $AppPoolName"
+$deploymentresult += Write-Output "Public(Path): $Public"
+$deploymentresult += Write-Output "PSEmailServer(SMTP Server): $PSEmailServer"
+$deploymentresult += Write-Output "Old_build_folder: $old_build_folder"
+$deploymentresult += Write-Output "New_build_folder: $new_build_folder`n"
+#$deploymentresult += Write-Output "`nYou entered:`nOld Build Folder: $old_build_folder"
+#$deploymentresult += Write-Output "New Build Folder: $new_build_folder"
+
+#
+###########     FUNCTIONS     ###########
+#
+
+# Convert the variable's output to string & send email
+
+Function SendEmail($deploymentresult)
+    {
+    $deploymentresult = Out-String -InputObject $deploymentresult
+    Send-MailMessage -To "ojas@onewire.com" -From "ojas@onewire.com" -Subject "Production Deployment result" -Body $deploymentresult
+    }
+
+#
+###########     Make sure that old_builds_folder or new_builds_folder variables aren't empty     ###########
+#
+
+if (($old_build_folder.Length -eq 0) -or ($new_build_folder.Length -eq 0))
+    {
+        $deploymentresult += "`nHalting script, either old_script_folder or new_build_folder is empty!"
+        SendEmail $deploymentresult
+        Exit
+    }
+    
 
 #
 ###########     Copy Public Stuff -- Web.config, client_secret.json     ###########
@@ -136,12 +180,15 @@ Restart-WebAppPool $AppPoolName
 $deploymentresult += "`nChecking if website is up..."
 
 [System.Net.HttpWebRequest]$HTTP_Request = [System.Net.WebRequest]::Create($WebsitePath)
+$HTTP_Request.UserAgent = "Mozilla/5.0";
+$HTTP_Request.Accept="text/html";
 $HTTP_Request.Method="HEAD"
 
 try
     {
-    [System.net.httpWebResponse]$HTTP_Response = $HTTP_Request.GetResponse() 
-    } 
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+	[System.net.httpWebResponse]$HTTP_Response = $HTTP_Request.GetResponse() 
+	} 
 catch [System.Net.WebExceptionStatus]
     {
     $HTTP_Response = $_.Exception.Response
@@ -177,20 +224,18 @@ $HTTP_Response.Close()
 
 if ($globalerror -eq 0)
     {
-    $deploymentresult += Write-Output  "`nOperation completed without any errors!"
+    $deploymentresult += Write-Output  "`nOperation completed without any errors! The $SiteName website is now running from $new_build_folder folder."
     }
 elseif ($globalerror -gt 0)
     {
-    $deploymentresult += Write-Output "`OPERATION COMPLETED WITH ERRORS, SEE ABOVE."
+    $deploymentresult += Write-Output "`OPERATION COMPLETED WITH ERRORS!! SEE ABOVE."
     }
 
 #
 ###########     Convert the variable's output to string & send email     ###########
 #
 
-$deploymentresult = Out-String -InputObject $deploymentresult
-
-Send-MailMessage -To "me@me.com" -From "me@me.com" -Subject "Deployment result" -Body $deploymentresult
+SendEmail $deploymentresult
 
 
 <#
