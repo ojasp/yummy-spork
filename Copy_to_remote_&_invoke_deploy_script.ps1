7f65fdaf8ff00dd2e92912840a82dea9eba6cf0a
ojasp <opanwar@gmail.com>
2015-11-09 17:40:49 -0500

    Update Copy_to_remote_&_invoke_deploy_script.ps1

--- Copy_to_remote_&_invoke_deploy_script.ps1
+++ Copy_to_remote_&_invoke_deploy_script.ps1
@@ -1,12 +1,12 @@
-$pass = "Password1" | ConvertTo-SecureString -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential('fsvs\otest',$pass)
+$pass = "MyPassword" | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential('Domain\user',$pass)
 #Import-Module TaskScheduler
 
 $copyerror =@()
-New-PSDrive -Name T -PSProvider FileSystem -Root \\192.168.22.41\c$\Test -Credential $cred
-Copy-Item C:\test\AdobeReaderDCMSI -Recurse T:\ -Force -ErrorAction SilentlyContinue -ErrorVariable copyerror   ##### Remove -Force switch if you want to exit when the folder exists in destination
+New-PSDrive -Name T -PSProvider FileSystem -Root \\192.168.2.2\c$\Builds\Public -Credential $cred
+Copy-Item C:\Builds\Integration\Release1.1 -Recurse T:\ -Force -ErrorAction SilentlyContinue -ErrorVariable copyerror   ##### Remove -Force switch if you want to exit when the folder exists in destination
 Remove-PSDrive -Name T
-net use "\\192.168.22.41\c$\Test" /delete
+net use "\\192.168.2.2\c$\Builds\Public" /delete
 
 if($copyerror.Count -eq 0)
     {
@@ -21,11 +21,11 @@ if($copyerror.Count -eq 0)
             $old_build_folder = "",                         #<---------This variable saved the old build folder that is entered by user
             $new_build_folder = ""                          #<---------This variable saved the new build folder that is entered by user
             $public="",                                     #<---------Relative path pf public website; relative to build folder(old_build_folder/new_build_folder)
-            $PSEmailServer = "onewire-web06.ny.fsvs.com",   #<---------This variable sets SMTP server. It's a system variable.
+            $PSEmailServer = "smtp.google.com",             #<---------This variable sets SMTP server. It's a system variable.
             #>
-        Invoke-Command -ComputerName onewire-web06 `
+        Invoke-Command -ComputerName 192.168.2.2 `
             {`
-            &"C:\Users\opanwar\Desktop\Automate Production Deployment.ps1" `
+            &"C:\Deploy_Scripts\Automate Production Deployment.ps1" `
             -old_build_folder "C:\inetpub\wwwroot" `
             -new_build_folder "C:\inetpub\wwwroot\Test" `
             }
